c46484576f502d8dcf4c0d6ac50f2561ecd9dce9
LinkofHyrule89 <LinkofHyruleUO@gmail.com>
2018-04-27 00:03:54 -0600

    First Commit

---
+++ src/main.js
@@ -0,0 +1,40 @@
+//ToDo: Import vue and name it Vue
+import Vue from 'vue'
+//ToDo: Import vue-router and name it VueRouter
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+//ToDo: Import vue-resource and name it VueResource
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
+//ToDo: Import App.vue and name it App
+import App from './App.vue'
+
+//ToDo: Import routes.js and use the {} passing routes
+import { routes } from './routes'
+//ToDo: Import store/store.js and name it store
+import store from 'store/store.js'
+//ToDo Initialize VueRouter using Vue.use()
+//ToDo Initialize VueResource using Vue.use()
+
+Vue.http.options.root = //ToDo: Set Equal to your Firebase Database URL
+
+Vue.filter('currency', (value) => {
+  //ToDo: Return value.toLocalString(), Add $ sign before the value
+    return $value.toLocalString()
+});
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+    //ToDo: Set router mode to history
+    //ToDo: Pass routes constant imported from above
+});
+
+new Vue({
+  el: '#app',
+    router,
+    store,
+    //ToDo: Pass the router constant to vue application
+    //ToDo: Pass store constant to vue application,
+  render: h => h(App)
+});
